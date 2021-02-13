0f7548be5f8130225ca754d4fb45bce3ba0ef5a5
Aaryadeva <73352730+Aaryadeva@users.noreply.github.com>
2021-02-13 15:53:08 +0530

    Add files via upload

--- Mario/sketch.js
+++ Mario/sketch.js
@@ -16,28 +16,36 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(400,400)
-  mario=createSprite(50,320,20,20)
+  createCanvas(windowWidth,windowHeight)
+  mario=createSprite(10,height-100,20,20)
   mario.addAnimation("standing",mario_standingImg)
   mario.addAnimation("running",mario_runningImg)
-  ground=createSprite(200,370,800,65)
-  ground.addImage(groundImg)
-  ground.scale=0.75
-  obstacle=createSprite(200,320,20,10)
+  ground=createSprite(0,height-90,width*4,20)
+  ground.visible=true
+  
+  obstacle=createSprite(200,height-116,20,10)
   obstacle.addAnimation("plant",obstacleImg)
   obstacle.scale=0.75
   
-  brick1=new Brick(250,250,20,20)
+ // brick1=createSprite()
  
-  coin=new Coin(brick1.x,brick1.y,20,20)
+  
   enemyGroup=createGroup()
   enemyKillGroup=createGroup()
+  for(var i=0;i<width*8;i=i+500){
+    brick1=createSprite(i,height-200,20,20)
+    brick1.addImage(brickImg)
+    brickImg.scale=0.1
+  }
+  coin=new Coin(brick1.x,brick1.y,20,20)
 }
 
 function draw(){
   background(bg)
   brick1.display()
   camera.position.x=mario.x
+  ground.x=mario.x
+  
   //mario.debug=true
   //enemy.debug=true
   if(keyIsDown(RIGHT_ARROW)){
@@ -74,22 +82,26 @@ function draw(){
   }
   mario.velocityY=mario.velocityY+0.5
   mario.collide(ground)
-  mario.collide(brick1)
+  //mario.collide(brick1)
   mario.setCollider("rectangle",0,3,20,25)
   
   drawSprites()
   text("Coins: "+totalCoins,camera.position.x-180,10)
   //console.log(mouseY)
+  spawnEnemies()
 }
 function spawnEnemies(){
-  if(frameCount%100===0){
-  enemy=createSprite(300,340,20,20)
-  enemy.x=mario.x+80
+  if(frameCount%400===0){
+  enemy=createSprite(width,height-96,20,20)
+  enemy.velocityX=-1
   enemy.addImage(enemyImg)
   enemy.scale=0.2
   enemyKill=createSprite(enemy.x,enemy.y-35,20,5)
   enemyKill.visible=false
   enemy.setCollider("rectangle",-10,-50,100,100)
+  enemy.lifetime=width
+  enemyKill.lifetime=width
+  enemyKill.velocityX=-1
   enemyGroup.add(enemy)
   enemyKillGroup.add(enemyKill)
 
